a0614c206873633cb86cc10142b1abe526b85bbd
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-07-16 17:06:15 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'QiRub',         # How you named your package folder (MyLib)
   packages = ['QiRub'],   # Chose the same as "name"
-  version = '1.0.3',      # Start with a small number and increase it with every change you make
+  version = '1.0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Rubka Client Library',   # Give a short description about your library
   author = 'Host1let',                   # Type in your name
@@ -14,7 +14,10 @@ setup(
           "httpx",
           "pycryptodome",
           "fake_useragent",
-          "rich"
+          "rich",
+          "moviepy",
+          "Pillow",
+          "mutagen"
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
