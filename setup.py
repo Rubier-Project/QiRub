57597ac57ad5125177c32ca8cd86d4ce2c083a21
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-07-16 15:50:24 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
   install_requires=[            # I get to this in a second
           "httpx",
           "pycryptodome",
-          "fake_useragent"
+          "fake_useragent",
+          "rich"
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
